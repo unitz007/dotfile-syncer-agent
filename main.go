d22c3990ef3689c284c23120de017ac5e83f18d5
Dinneya Charles <dinneyacharles007@gmail.com>
2024-12-23 21:59:28 +0100

    fixed issues

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/r3labs/sse/v2"
-	"github.com/robfig/cron/v3"
 	"github.com/spf13/cobra"
 	"net/http"
 	"os"
@@ -14,18 +13,17 @@ import (
 
 func main() {
 	var (
-		cronJob          = cron.New()
-		rootCmd          = cobra.Command{}
-		mux              = http.NewServeMux()
-		sseServer        = sse.New()
-		brokerNotifier   = NewBrokerNotifier()
-		syncStatusStream = sseServer.CreateStream(SyncStatusLabel)
-		//syncTriggerStream = sseServer.CreateStream(SyncTriggerLabel)
-		port        = rootCmd.Flags().StringP("port", "p", DefaultPort, "HTTP port to run on")
-		webhookUrl  = rootCmd.Flags().StringP("webhook", "w", "", "git webhook url")
-		dotFilePath = rootCmd.Flags().StringP("dotfile-path", "d", "", "path to dotfile directory")
-		configDir   = rootCmd.Flags().StringP("config-dir", "c", "", "path to config directory")
-		gitUrl      = rootCmd.Flags().StringP("git-url", "g", "", "github api url")
+		rootCmd           = cobra.Command{}
+		mux               = http.NewServeMux()
+		sseServer         = sse.New()
+		brokerNotifier    = NewBrokerNotifier()
+		syncStatusStream  = sseServer.CreateStream(SyncStatusLabel)
+		syncTriggerStream = sseServer.CreateStream(SyncTriggerLabel)
+		port              = rootCmd.Flags().StringP("port", "p", DefaultPort, "HTTP port to run on")
+		webhookUrl        = rootCmd.Flags().StringP("webhook", "w", "", "git webhook url")
+		dotFilePath       = rootCmd.Flags().StringP("dotfile-path", "d", "", "path to dotfile directory")
+		configDir         = rootCmd.Flags().StringP("config-dir", "c", "", "path to config directory")
+		gitUrl            = rootCmd.Flags().StringP("git-url", "g", "", "github api url")
 	)
 
 	if err := rootCmd.Execute(); err != nil {
@@ -88,52 +86,49 @@ func main() {
 						syncStatusStream.Eventlog = []*sse.Event{event}
 					}
 
-					//localCommit, err := git.LocalCommit()
-					//if err != nil {
-					//	Error(err.Error())
-					//	return
-					//}
-					//
-					//remoteCommit, err := git.RemoteCommit()
-					//if err != nil {
-					//	Error(err.Error())
-					//	return
-					//}
-					//
-					//response := InitGitTransform(localCommit, remoteCommit)
-					//v, _ := json.Marshal(response)
-					//
-					//// first event
-					//if len(syncStatusStream.Eventlog) == 0 {
-					//	sseServer.Publish(SyncStatusLabel, &sse.Event{Data: v})
-					//	brokerNotifier.SyncStatus(response)
-					//	if !response.IsSync {
-					//		go func() {
-					//			go syncer.Sync()
-					//			syncer.Consume(ConsoleSyncConsumer)
-					//			syncTriggerStream.Eventlog.Clear()
-					//		}()
-					//	}
-					//}
-					//
-					//event := syncStatusStream.Eventlog[len(syncStatusStream.Eventlog)-1]
-					//
-					//var prevResponse SyncStatusResponse
-					//_ = json.Unmarshal(event.Data, &prevResponse)
-					//
-					//fmt.Println(response.IsSync, prevResponse.IsSync)
-					//
-					//if response.IsSync != prevResponse.IsSync {
-					//	sseServer.Publish(SyncStatusLabel, &sse.Event{Data: v})
-					//	brokerNotifier.SyncStatus(response)
-					//}
+					localCommit, err := git.LocalCommit()
+					if err != nil {
+						Error(err.Error())
+						return
+					}
+
+					remoteCommit, err := git.RemoteCommit()
+					if err != nil {
+						Error(err.Error())
+						return
+					}
+
+					response := InitGitTransform(localCommit, remoteCommit)
+					if !response.IsSync {
+						go func() {
+							go syncer.Sync()
+							syncer.Consume(ConsoleSyncConsumer)
+							syncTriggerStream.Eventlog.Clear()
+						}()
+					}
+					v, _ := json.Marshal(response)
+
+					// first event
+					if len(syncStatusStream.Eventlog) == 0 {
+						sseServer.Publish(SyncStatusLabel, &sse.Event{Data: v})
+						brokerNotifier.SyncStatus(response)
+					} else {
+
+						event := syncStatusStream.Eventlog[len(syncStatusStream.Eventlog)-1]
+
+						var prevResponse SyncStatusResponse
+						_ = json.Unmarshal(event.Data, &prevResponse)
+
+						if response.IsSync != prevResponse.IsSync {
+							sseServer.Publish(SyncStatusLabel, &sse.Event{Data: v})
+							brokerNotifier.SyncStatus(response)
+						}
+					}
 				})
 			}()
 			time.Sleep(5 * time.Second)
 		}
 	}()
-
-	cronJob.Start()
 
 	// register handlers
 	mux.HandleFunc("/sync", syncHandler.Sync)
